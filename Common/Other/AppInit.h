31f94f9428aa1dff4a67957ceebbf1e22bd177cf
邓顺来 <mu3305495@163.com>
2017-06-21 17:05:12 +0800

    coding
    coding

---
+++ Common/Other/AppInit.h
@@ -0,0 +1,15 @@
+//
+//  AppInit.h
+//  Common
+//
+//  Created by dengshunlai on 2017/6/21.
+//  Copyright © 2017年 邓顺来. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppInit : NSObject
+
++ (void)init;
+
+@end
---
+++ Common/Other/AppInit.m
@@ -0,0 +1,19 @@
+//
+//  AppInit.m
+//  Common
+//
+//  Created by dengshunlai on 2017/6/21.
+//  Copyright © 2017年 邓顺来. All rights reserved.
+//
+
+#import "AppInit.h"
+#import "HTTPRequest.h"
+#import "AppAPI.h"
+
+@implementation AppInit
+
++ (void)init {
+    ;
+}
+
+@end
